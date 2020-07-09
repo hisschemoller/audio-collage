20053297e53efb9cbcd0d479b107991e7d9e5b2a
Wouter Hisschemöller <wouter.hisschemoller@digitas.com>
2020-07-09 18:27:22 +0200

    Node reading files in directories, WIP.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -15,5 +17,52 @@ app.listen(port, () => {
 
 app.post('/paths', (req, res) => {
   console.log(req.body);
+  getAllDirectories(req.body);
   res.send(req.body);
 });
+
+const allData = [];
+let numImages = 0;
+
+/**
+ * 
+ * @param {*} dirs 
+ */
+function getAllDirectories(dirs) {
+  Promise.all(dirs.map(getAllFiles)).then(() => {
+    numImages = 0;
+    allData.forEach(dirData => {
+      dirData.startIndex = numImages;
+      numImages += dirData.images.length;
+      console.log(`dir data: ${dirData.startIndex} - ${dirData.images.length} - ${numImages}`);
+    });
+    console.log('num files', numImages);
+  });
+}
+
+/**
+ * 
+ * @param {*} dir 
+ */
+function getAllFiles(dir) {
+  return new Promise((resolve, reject) => {
+    let images = [];
+    fs.readdir(dir, (err, files) => {
+      if (err) {
+        console.log('err', err);
+        reject();
+      } else {
+        files.forEach(filename => {
+          if ((/[^\s]+(\.(mp3|wav|aif))$/i).test(filename)) {
+            images.push(filename);
+          }
+        });
+        allData.push({
+          dir: dir,
+          images: images
+        });
+        resolve();
+      }
+    });
+  });
+}
